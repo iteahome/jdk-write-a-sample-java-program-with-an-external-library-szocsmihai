650e5fb51c38d5fe2bf1cb64a52f3be943e3aaf3
Mihai Szocs <57562487+szocsmihai@users.noreply.github.com>
2019-12-03 07:13:05 +0200

    Simple java program with an external library
    ITea Home Academy assignment added to GitHub via upload

---
+++ MisusNewGreeting.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class MisusNewGreeting {
+    public static void main(String[] args) {
+        String x = "and a Happy New Year! Also Misu.";
+        System.out.println(StringUtils.capitalize(x));
+    }
+}
